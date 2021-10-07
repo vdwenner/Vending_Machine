83983ce62b330532c577cac3244803a13280a8af
Vaughn Wenner <vdwenner@gmail.com>
2021-10-07 13:08:33 -0400

    trying to get the file input

--- java/src/main/java/com/techelevator/VendingMachine.java
+++ java/src/main/java/com/techelevator/VendingMachine.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class VendingMachine {
     public void getVendingInfo () {
-
-        File vendingFile = new File("vendingmachine.csv");
+        String fileName = "vendingmachine.csv";
+        String directPath = fileName.get;
+        File vendingFile = new File("C:/Users/Student/pair-projects/green-mod1-capstone-pair-team0/java/vendingmachine.csv");
         Scanner input = new Scanner(vendingFile);
     }
 }
