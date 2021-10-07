94275edd8c0297b3e9cbcaedd3d0ce10c61b2099
Savannah Guffey <1800savannahbaines@gmail.com>
2021-10-07 15:31:48 -0400

    created slot map

--- java/src/main/java/com/techelevator/VendingMachine.java
+++ java/src/main/java/com/techelevator/VendingMachine.java
@@ -1,6 +1,7 @@
 package com.techelevator;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import static java.lang.System.exit;
@@ -11,8 +12,16 @@ public class VendingMachine {
 
 
         File vendingFile = new File("vendingmachine.csv");
-        try (Scanner input = new Scanner(vendingFile)){
-
+        HashMap<String, Slot> slotMap = new HashMap<>();
+        try (Scanner fileInput = new Scanner(vendingFile)){
+            while(fileInput.hasNextLine()) {
+                String line = fileInput.nextLine();
+                if ((line != null) && (!line.isEmpty())) {
+                    String[] parts = line.split("\\|");
+                    String slotName = parts[0];
+                    slotMap.put(slotName, new Slot(line));
+                }
+            }
         }
         catch (FileNotFoundException e){
             System.out.println("File not found, please try again.");
