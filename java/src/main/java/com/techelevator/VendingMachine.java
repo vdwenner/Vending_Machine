58a0aa5d01e29714f00e462ce2bc701a5f4e9bb8
Savannah Guffey <1800savannahbaines@gmail.com>
2021-10-07 13:18:07 -0400

    stuck finding file

--- java/src/main/java/com/techelevator/VendingMachine.java
+++ java/src/main/java/com/techelevator/VendingMachine.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class VendingMachine {
     public void getVendingInfo () {
-        String fileName = "vendingmachine.csv";
-        String directPath = fileName.get;
-        File vendingFile = new File("C:/Users/Student/pair-projects/green-mod1-capstone-pair-team0/java/vendingmachine.csv");
+
+        File vendingFile = new File("vendingmachine.csv");
         Scanner input = new Scanner(vendingFile);
+
+        
+
     }
 }
