cfdc5ee53dba7721d10cc95afeec46e508991e81
Vaughn Wenner <vdwenner@gmail.com>
2021-10-07 17:16:17 -0400

    beginning methods role switch

--- java/src/main/java/com/techelevator/VendingMachine.java
+++ java/src/main/java/com/techelevator/VendingMachine.java
@@ -7,12 +7,24 @@ import java.util.Scanner;
 import static java.lang.System.exit;
 
 public class VendingMachine {
+    public HashMap<String, Slot> slotMap = new HashMap<>();
+
+    public String makeChange(int balance){
+        int nickel = 5;
+        int dime = 10;
+        int quarter = 25;
+        int leftOver = 0;
+        int coin = quarter;
+        if (balance >= coin){
+            leftOver = balance % quarter;
+        }
+    }
+
     public void getVendingInfo () {
 
 
 
         File vendingFile = new File("vendingmachine.csv");
-        HashMap<String, Slot> slotMap = new HashMap<>();
         try (Scanner fileInput = new Scanner(vendingFile)){
             while(fileInput.hasNextLine()) {
                 String line = fileInput.nextLine();
@@ -27,8 +39,11 @@ public class VendingMachine {
             System.out.println("File not found, please try again.");
             exit(1);
         }
-
-
-
     }
+        public HashMap<String, Slot> getSlotMap() {
+            return slotMap;
+        }
+
+
+
 }
